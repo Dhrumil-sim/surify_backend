5cc3c9f24239b5b47237a3153493971cdafc4602
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-05 12:11:17 +0530

    feat: jsdoc and docdash configuration

---
+++ src/index.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * Adds two numbers together.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of `a` and `b`.
+ */
+function add(a, b) {
+    return a + b;
+}
+/**
+ * Adds two numbers together.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of `a` and `b`.
+ */
+function minus(a, b) {
+    return a + b;
+}
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+  /**
+   * Adds two numbers together.
+   * @param {number} a - The first number.
+   * @param {number} b - The second number.
+   * @returns {number} The sum of `a` and `b`.
+   */
+  function add(a: number, b: number): number {
+    return a + b;
+  }
+  
+  /**
+   * Adds two numbers together.
+   * @param {number} a - The first number.
+   * @param {number} b - The second number.
+   * @returns {number} The sum of `a` and `b`.
+   */
+  function minus(a: number, b: number): number {
+    return a + b;
+  }
+  
