c63a506c18e35eb48bd42a26864d9666c7d55627
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-07 10:37:58 +0530

    feat: configuration for cors and async handler utility

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,17 @@
+/**
+ * @module Utils
+ * @description Utility functions for handling async operations and common tasks.
+ */
+
+/**
+ * Handles async route handlers by wrapping them in a try-catch.
+ * @function asyncHandler
+ * @memberof module:Utils
+ * @param {Function} requestHandler - The async function to handle requests.
+ * @returns {Function} Express middleware function that handles errors.
+ */
+const asyncHandler = (requestHandler: Function) => {
+    return (req: Object, res: Object, next: any) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    };
+};
