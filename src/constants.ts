eb943604130b8249762e692d8963cb0d0dbc5c88
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-07 11:03:53 +0530

    feat: added the different types of error objects

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,10 @@
 export const DB_NAME: string = "surify";
+
+
+export const HTTP_ERRORS = {
+    BAD_REQUEST: { code: 400, type: "Bad Request" },
+    UNAUTHORIZED: { code: 401, type: "Unauthorized" },
+    FORBIDDEN: { code: 403, type: "Forbidden" },
+    NOT_FOUND: { code: 404, type: "Not Found" },
+    INTERNAL_SERVER_ERROR: { code: 500, type: "Internal Server Error" },
+};
