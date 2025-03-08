99f8b1d7e6451a0bc7839f9d2931845b74124558
your-github-username <your-github-email>
2025-03-08 22:17:27 +0530

    feat: Notification module is created

---
+++ src/models/notification.model.ts
@@ -0,0 +1,28 @@
+import mongoose ,{Schema} from "mongoose";
+
+const notificationSchema = new Schema(
+    
+    {
+       user_id : {
+            type: Schema.Types.ObjectId,
+            required: true,
+       },
+
+       message:{
+           type:String,
+       },
+
+       read:{
+         type: Boolean,
+       },
+
+       createdAt: {
+        type: Date,
+       }
+
+    },{
+        timestamps: true,
+    }
+);
+
+export const Notification = mongoose.model("Notification",notificationSchema);
