b74c2e269f644683ba312da66360a5b63c8e420b
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-10 14:48:02 +0530

    fix: in index.js imported app.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import connectDB from "./db/index.js";
+import express from "express";
+import { app } from './app.js';
 
-connectDB();
+const PORT = process.env.PORT || 3000;
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
