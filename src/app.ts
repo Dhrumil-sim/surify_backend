3ac4613502550a60e8496f94f83fc0c9e5504525
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-10 14:48:39 +0530

    feat: added api/user route in app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,4 +13,15 @@ app.use(cors({
 app.use(express.json({limit:'16kb'}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+import userRouter from './routes/user.routes.js';
+
+
+app.use("api/user",userRouter);
+
+app.get('/', (req, res) => {
+    res.send("Hello ");
+});
+
+
 export {app};
