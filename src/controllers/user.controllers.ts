f31d6546f085ebddb27c36b16b877f4f377f758d
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-11 14:42:05 +0530

    feat: validation for all required field is done

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -3,24 +3,14 @@ import { ApiError } from '../utils/ApiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 const registerUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { username, email, password, joining_date, role } = req.body;
+    const { username, email, password,role } = req.body;
 
-    if (
-        [username,email,password,role].some((field)=>{
-
-            field?.trim()===""
-        }) ) {
+    if ([username,email,password,role].some((field)=>field?.trim()==="")) 
+    {
         // Throwing a 400 Bad Request error with a specific error code
         throw new ApiError(400, 'ALL_IS_REQUIRED', 'All fields are required');
     }
 
-    // Additional user registration logic here
-
-    res.status(201).json({
-        success: true,
-        message: 'User registered successfully',
-        data: { username, email, role },
-    });
 });
 
 export { registerUser };
