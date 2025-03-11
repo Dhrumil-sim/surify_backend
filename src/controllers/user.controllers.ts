3b88cf187ee28635f12c7eaf181fc1b088e54dbb
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-11 14:51:19 +0530

    feat: Validation for already existed user with given email or username validation is done

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { ApiError } from '../utils/ApiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
+import { User } from '../models/user.model.js';
 
 const registerUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const { username, email, password,role } = req.body;
@@ -10,6 +11,13 @@ const registerUser = asyncHandler(async (req: Request, res: Response, next: Next
         // Throwing a 400 Bad Request error with a specific error code
         throw new ApiError(400, 'ALL_IS_REQUIRED', 'All fields are required');
     }
+
+
+     // it will check that user is already exists or not with either given email or username
+
+    const existedUser = User.findOne({
+        $or: [{ username },{email}]
+    })
 
 });
 
