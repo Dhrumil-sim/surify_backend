d5edae595524065fcda25bf04701e2e2dc6e795e
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-19 10:50:27 +0530

    fix: console is fixed

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ console.log(typeof process.env.ACCESS_TOKEN_EXPIRY);
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+        console.log(`⚙️ Server is running at port : http://localhost:${process.env.PORT}`);
     })
 })
 .catch((err) => {
