c39ed564eba8be144ff96356497ca69693ee59db
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-28 14:52:49 +0530

    fix: console and app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 import app from './app.js';
-import { console } from 'inspector';
 dotenv.config();
 
-const PORT = process.env['PORT'] || 3000;
+const PORT = process.env.PORT || 3000;
 console.log('hello');
 
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
       console.log(`⚙️ Server is running at: http://localhost:${PORT}`);
-      console.log('Hello');
-      console.log('Hello');
     });
   })
   .catch((err) => {
