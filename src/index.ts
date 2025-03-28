4b60959405c0bebab680b24366e65392f860b17a
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-28 14:01:19 +0530

    fix: quote

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
-import app from "./app.js";
-import { Request, Response } from "express";
-import { console } from "inspector";
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import app from './app.js';
+import { Request, Response } from 'express';
+import { console } from 'inspector';
 dotenv.config();
 
-const PORT = process.env["PORT"] || 3000;
-console.log("hello");
+const PORT = process.env['PORT'] || 3000;
+console.log('hello');
 
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
       console.log(`⚙️ Server is running at: http://localhost:${PORT}`);
-      console.log("Hello");
+      console.log('Hello');
+      console.log('Hello');
     });
   })
   .catch((err) => {
-    console.error("❌ MongoDB connection failed!", err);
+    console.error('❌ MongoDB connection failed!', err);
   });
