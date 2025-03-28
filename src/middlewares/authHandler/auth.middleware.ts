bc5f88ba4ec0e26d7079942ad56eb67fe6a70357
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-28 14:12:34 +0530

    fix: prettier format

--- src/middlewares/authHandler/auth.middleware.ts
+++ src/middlewares/authHandler/auth.middleware.ts
@@ -2,11 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import { asyncHandler } from '../../utils/asyncHandler.js';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { User } from '../../models/user.model.js';
+import mongoose from 'mongoose';
 
 // Extend Express Request to include cookies
 interface AuthenticatedRequest extends Request {
   cookies: { accessToken?: string }; // Define cookies with accessToken
-  user?: any;
+  user?: mongoose.Document;
 }
 
 export const verifyJWT = asyncHandler(
@@ -22,27 +23,25 @@ export const verifyJWT = asyncHandler(
         .json({ message: 'Unauthorized: No token provided' });
     }
 
-    const secret: any = process.env.ACCESS_TOKEN_SECRET;
+    const secret: string = process.env['ACCESS_TOKEN_SECRET']!;
     if (!secret) {
-      return res
-        .status(500)
-        .json({
-          message: 'Internal Server Error: Missing ACCESS_TOKEN_SECRET',
-        });
+      return res.status(500).json({
+        message: 'Internal Server Error: Missing ACCESS_TOKEN_SECRET',
+      });
     }
 
     try {
       // Verify token and cast it to JwtPayload
       const decodedToken = jwt.verify(token, secret) as JwtPayload;
 
-      if (!decodedToken._id) {
+      if (!decodedToken['_id']) {
         return res
           .status(401)
           .json({ message: 'Unauthorized: Invalid token structure' });
       }
 
       // Find user by ID from token payload
-      const user = await User.findById(decodedToken._id).select(
+      const user = await User.findById(decodedToken['_id']).select(
         '-password -refreshToken'
       );
       if (!user) {
@@ -54,7 +53,7 @@ export const verifyJWT = asyncHandler(
       req.user = user;
 
       next(); // Proceed to next middleware
-    } catch (error) {
+    } catch {
       return res.status(401).json({ message: 'Unauthorized: Invalid token' });
     }
   }
