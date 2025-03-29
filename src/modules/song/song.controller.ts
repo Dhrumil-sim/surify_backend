8c04c978808b718f82ffcb879b74a900895ca782
your-github-username <your-github-email>
2025-03-29 15:42:09 +0530

    fix: improve and add some functionalities which helps to build the album module

--- src/modules/song/song.controller.ts
+++ src/modules/song/song.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Request, Response, NextFunction } from 'express';
 import { asyncHandler } from '../../utils/asyncHandler.js';
 import SongMetaData from './utils/songMetadata.util.js';
