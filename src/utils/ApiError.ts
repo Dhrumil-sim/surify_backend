d23d682b4156452ba54fb0da5a35f917a88bc60b
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-04-01 15:51:33 +0530

    fix: added data field in the error format

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -1,25 +1,31 @@
 import { StatusCodes, getReasonPhrase } from 'http-status-codes';
 
+interface ErrorDetail {
+  field: string;
+  message: string;
+}
+
 class ApiError extends Error {
   statusCode: number;
-  data: any;
-  success: boolean;
-  errors: any[];
   errorCode: string;
+  success: boolean;
+  errors: ErrorDetail[];
+  data: unknown; // Can be used to store any type of response data (specific to your needs)
 
   constructor(
     statusCode: number = StatusCodes.INTERNAL_SERVER_ERROR,
     errorCode: string = 'INTERNAL_ERROR',
     message: string = getReasonPhrase(statusCode),
-    errors: any[] = [],
+    errors: ErrorDetail[] = [],
+    data: unknown = null, // The data field can hold any value, but specifying `unknown` is safer than `any`
     stack: string = ''
   ) {
     super(message);
     this.statusCode = statusCode;
     this.errorCode = errorCode;
-    this.data = null;
     this.success = false;
     this.errors = errors;
+    this.data = data;
 
     if (stack) {
       this.stack = stack;
