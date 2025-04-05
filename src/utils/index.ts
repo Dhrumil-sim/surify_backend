7a5831b83fa46a786660db014ccb743ffa3ecbda
your-github-username <your-github-email>
2025-04-05 11:27:02 +0530

    fix: husky test

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ import { ApiError } from './ApiError.js';
 import { asyncHandler } from './asyncHandler.js';
 import { ApiResponse } from './ApiResponse.js';
 export { ApiError, asyncHandler, ApiResponse };
+const a = 10;
