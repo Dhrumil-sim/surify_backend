873bfbd465d421c551cc7d29885edf89795b3600
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-04-08 14:23:13 +0530

    fix: at the time of the album creation id of the album was not added to the song

--- src/modules/album/services/album.service.ts
+++ src/modules/album/services/album.service.ts
@@ -1,5 +1,5 @@
-import { Album } from '@models';
-import { ISong, SongMetaData, SongService } from '@songModule';
+import { Album, Song } from '@models';
+import { ISong, SongFileHash, SongMetaData } from '@songModule';
 import mongoose from 'mongoose';
 import { IAlbum } from '@albumModule';
 
@@ -22,9 +22,12 @@ export class AlbumService {
         const coverPath = songCovers[index];
 
         let duration = 0;
+        let fileHashVal = '';
         try {
           const metadata = await SongMetaData.getMetadata(filePath);
+          const fileHash = await SongFileHash.fileHash(filePath);
           duration = metadata.format.duration || 0;
+          fileHashVal = fileHash;
         } catch {
           console.warn(`Metadata failed for ${filePath}`);
         }
@@ -35,6 +38,7 @@ export class AlbumService {
           coverPicture: coverPath,
           filePath,
           releaseDate: new Date(),
+          fileHash: fileHashVal,
           duration,
         };
       })
@@ -50,16 +54,12 @@ export class AlbumService {
 
     const createdSongs = await Promise.all(
       songsWithFiles.map(async (song) => {
-        const newSong = await SongService.createSong(
-          userId,
-          song.title,
-          song.genre,
-          new Date(),
-          song.duration,
-          song.coverPicture,
-          song.filePath,
-          newAlbum._id.toString()
-        );
+        const newSong = await Song.create({
+          ...song,
+          artist: new mongoose.Types.ObjectId(userId),
+
+          album: newAlbum._id.toString(),
+        });
         return newSong._id;
       })
     );
@@ -68,6 +68,31 @@ export class AlbumService {
     await newAlbum.save();
 
     return newAlbum;
+  }
+
+  static async updateAlbum(
+    albumId: mongoose.Types.ObjectId,
+    updates: Partial<IAlbum>,
+    existingAlbum: IAlbum
+  ): Promise<IAlbum> {
+    const { title, genre, songs, coverPicture, songFiles, songCovers } =
+      updates;
+
+    // Merge existing with new
+    const updatedData: Partial<IAlbum> = {
+      title: title || existingAlbum.title,
+      genre: genre || existingAlbum.genre,
+      coverPicture: coverPicture || existingAlbum.coverPicture,
+      songFiles: songFiles || existingAlbum.songFiles,
+      songCovers: songCovers || existingAlbum.songCovers,
+      songs: songs || existingAlbum.songs,
+    };
+
+    const updatedAlbum = await Album.findByIdAndUpdate(albumId, updatedData, {
+      new: true,
+    });
+
+    return updatedAlbum as IAlbum;
   }
 
   static async getArtistAlbum(artistId: IAlbum['id']): Promise<IAlbum[]> {
