069e0f9a9bf4c8011ebebcd29090180b9fdc2409
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-04-09 10:03:55 +0530

    fix: added modular utils for the field parser

--- src/routes/album.routes.ts
+++ src/routes/album.routes.ts
@@ -1,50 +1,17 @@
-import { NextFunction, Router } from 'express';
+import { Router } from 'express';
 import { verifyJWT } from '@userModule';
 import { validateRequest } from '@middlewares';
 import {
   uploadAlbum,
   albumSchema,
   AlbumController,
-  AuthenticatedRequest,
-  AlbumService,
+  AlbumFieldParser,
 } from '@albumModule';
-import { Response } from 'express';
-import { asyncHandler } from '@utils';
 import { albumUpdateValidator, albumCreateValidator } from '@albumModule';
-import mongoose from 'mongoose';
-import { SongService } from '@songModule';
 import { albumUpdateSchema } from 'modules/album/utils/albumAndSongValidation';
 
 const router = Router();
 
-// 🔹 Middleware to parse JSON fields before Joi and controller
-export const parseAlbumFields = asyncHandler(
-  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-    const albumId = new mongoose.Types.ObjectId(req.params.albumId);
-    const originalSong = await SongService.getSongByAlbumId('' + albumId);
-    const originalAlbum = await AlbumService.getAlbumById(albumId);
-    console.log(originalAlbum);
-    if (req.body.songs && typeof req.body.songs === 'string') {
-      req.body.songs = JSON.parse(req.body.songs);
-    } else {
-      const simplifiedSongs = originalSong['genre'];
-
-      req.body.songs = simplifiedSongs;
-    }
-
-    if (req.body.genre && typeof req.body.genre === 'string') {
-      req.body.genre = JSON.parse(req.body.genre);
-    } else {
-      const originalGenre = originalAlbum['genre'];
-      req.body.genre = originalGenre;
-    }
-    if (!req.files?.coverPicture) {
-      req.body.coverPicture = originalAlbum['coverPicture'];
-    }
-
-    next(); // manually call next after all async ops
-  }
-);
 router.post(
   '/create',
   verifyJWT,
@@ -53,7 +20,7 @@ router.post(
     { name: 'songFiles', maxCount: 10 },
     { name: 'songCovers', maxCount: 10 },
   ]),
-  parseAlbumFields,
+  AlbumFieldParser.createParseField,
   validateRequest(albumSchema), // Joi-level validation
   albumCreateValidator, // Our custom utility
   AlbumController.createAlbum
@@ -68,7 +35,7 @@ router.put(
     { name: 'songCovers', maxCount: 10 },
   ]),
 
-  parseAlbumFields,
+  AlbumFieldParser.updateParseField,
   validateRequest(albumUpdateSchema),
   albumUpdateValidator,
 
@@ -79,5 +46,6 @@ router.put(
 router.get('/', verifyJWT, AlbumController.getArtistAlbums);
 router.get('/get/allAlbums', verifyJWT, AlbumController.getAllAlbums);
 router.get('/album-by-id/:albumId', verifyJWT, AlbumController.getAlbumById);
+router.delete('/:albumId', verifyJWT, AlbumController.deleteAlbum);
 
 export default router;
